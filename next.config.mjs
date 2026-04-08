ec379d70d354e8f0c439689f247faf70beef664f
Khysamo <Khysamo@gmail.com>
2026-04-08 15:34:22 -0500

    cambios
    sasas

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // Reemplaza 'Portafolio_Samuel_Perez_Betancur' por el nombre de tu repositorio en GitHub
+  basePath: '/Portafolio_Samuel_Perez_Betancur',
   typescript: {
     ignoreBuildErrors: true,
   },
