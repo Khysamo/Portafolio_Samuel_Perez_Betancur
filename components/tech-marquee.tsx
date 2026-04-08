f77c813db4c2bedb3e21fba18b53839a2fe0e358
Khysamo <Khysamo@gmail.com>
2026-04-08 15:51:56 -0500

    asas

--- components/tech-marquee.tsx
+++ components/tech-marquee.tsx
@@ -4,7 +4,7 @@ import {
   SiUnity,
   SiSharp,
   SiHtml5,
-  SiCss3,
+  SiCss,
   SiJavascript,
   SiGit,
   SiGithub,
@@ -12,17 +12,17 @@ import {
 } from "react-icons/si"
 
 const row1 = [
-  { icon: SiUnity,        name: "Unity",          color: "#FFFFFF" },
-  { icon: SiSharp,        name: "C#",             color: "#9B4F97" },
-  { icon: SiHtml5,        name: "HTML",           color: "#E34F26" },
-  { icon: SiCss3,         name: "CSS",            color: "#1572B6" },
+  { icon: SiUnity, name: "Unity", color: "#FFFFFF" },
+  { icon: SiSharp, name: "C#", color: "#9B4F97" },
+  { icon: SiHtml5, name: "HTML", color: "#E34F26" },
+  { icon: SiCss, name: "CSS", color: "#1572B6" },
 ]
 
 const row2 = [
-  { icon: SiJavascript,   name: "JavaScript",     color: "#F7DF1E" },
-  { icon: SiGit,          name: "Git",            color: "#F05032" },
-  { icon: SiGithub,       name: "GitHub",         color: "#FFFFFF" },
-  { icon: SiFigma,        name: "Figma",          color: "#F24E1E" },
+  { icon: SiJavascript, name: "JavaScript", color: "#F7DF1E" },
+  { icon: SiGit, name: "Git", color: "#F05032" },
+  { icon: SiGithub, name: "GitHub", color: "#FFFFFF" },
+  { icon: SiFigma, name: "Figma", color: "#F24E1E" },
 ]
 
 function MarqueeRow({
@@ -77,7 +77,7 @@ function MarqueeRow({
 export function TechMarquee() {
   return (
     <div className="space-y-4">
-      <MarqueeRow items={row1} direction="left"  speed={30} />
+      <MarqueeRow items={row1} direction="left" speed={30} />
       <MarqueeRow items={row2} direction="right" speed={28} />
     </div>
   )
